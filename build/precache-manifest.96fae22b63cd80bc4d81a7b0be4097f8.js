b46b41392a8aecef3a707f63f1314c2a2e400ef6
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-10-02 14:39:35 -0400

    Add files via upload
    Began working on improving website speed. Improved desktop from 82 to 98. Improved mobile from 59 to 75. 
    Changed png images to webp.
    Reduced video length and quality by 2 mbs.
    On screens 768px disabled video and added background img instead.

---
+++ build/precache-manifest.96fae22b63cd80bc4d81a7b0be4097f8.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "e78c3ea7ea7c1ce81790",
+    "url": "/static/js/main.e78c3ea7.chunk.js"
+  },
+  {
+    "revision": "afc60855bda1c69a4c39",
+    "url": "/static/js/1.afc60855.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "881df17d9d0b0785e0f0fdab007e735d",
+    "url": "/static/media/shortWater.881df17d.mp4"
+  },
+  {
+    "revision": "dd2f95d25f24afdd79b661fb0cb47702",
+    "url": "/static/media/water.dd2f95d2.webp"
+  },
+  {
+    "revision": "ba51d754fc00cf2e575c60856f7074c0",
+    "url": "/static/media/orionpalmer.ba51d754.webp"
+  },
+  {
+    "revision": "91a9759734574dbd3539057557d9c6e2",
+    "url": "/static/media/Constellation.91a97597.webp"
+  },
+  {
+    "revision": "0169c584881355416b667b8626d15c85",
+    "url": "/static/media/react1.0169c584.webp"
+  },
+  {
+    "revision": "0a1f65709bf563a145303801621fac66",
+    "url": "/static/media/javascript.0a1f6570.webp"
+  },
+  {
+    "revision": "86eab88f88f3f3a605d71f1ffe9cad47",
+    "url": "/static/media/sass1 (1).86eab88f.webp"
+  },
+  {
+    "revision": "af190da836a6dce00b2dabac9d451517",
+    "url": "/static/media/css.af190da8.webp"
+  },
+  {
+    "revision": "40c0769244ef9e425780cc894041c820",
+    "url": "/static/media/html.40c07692.webp"
+  },
+  {
+    "revision": "a20b783eefaa55fb80a32ebfee804a12",
+    "url": "/static/media/jquery.a20b783e.webp"
+  },
+  {
+    "revision": "7e5cdd92a6f2a07e39a580bac0db719f",
+    "url": "/static/media/bootstrap.7e5cdd92.webp"
+  },
+  {
+    "revision": "28d849e5821230227241a6020e638016",
+    "url": "/static/media/api.28d849e5.webp"
+  },
+  {
+    "revision": "8dfcde3abfab163ca8510c8c557bb598",
+    "url": "/static/media/node.8dfcde3a.webp"
+  },
+  {
+    "revision": "8ee500aa84e54a2065a6b16146516f29",
+    "url": "/static/media/express.8ee500aa.webp"
+  },
+  {
+    "revision": "09b441fa8f21b6f2b3041ba7601af78d",
+    "url": "/static/media/git.09b441fa.webp"
+  },
+  {
+    "revision": "710947f4eb08fc48160ab6d92a8e9e12",
+    "url": "/static/media/github.710947f4.webp"
+  },
+  {
+    "revision": "cfc85015264307c003ff0ad2e5e26773",
+    "url": "/static/media/stackoverflow.cfc85015.webp"
+  },
+  {
+    "revision": "be51947c0716d425bb912b92275e2252",
+    "url": "/static/media/vscode.be51947c.webp"
+  },
+  {
+    "revision": "4c27cf147cbaaedb71e1679877176b92",
+    "url": "/static/media/Adobe_XD_CC_icon-300x300.4c27cf14.webp"
+  },
+  {
+    "revision": "0917721e879d364142d6fe5a677882d8",
+    "url": "/static/media/beatmachine.0917721e.webp"
+  },
+  {
+    "revision": "0baf6198c3d843329ba7bbdbb1a0dbd9",
+    "url": "/static/media/Pokedex.0baf6198.webp"
+  },
+  {
+    "revision": "70b7752160e6ba3faf3a91baa44300a8",
+    "url": "/static/media/Keeper-App.70b77521.webp"
+  },
+  {
+    "revision": "9986a7eeb726a41fbc8d8933306c7dfe",
+    "url": "/static/media/portfolio.9986a7ee.webp"
+  },
+  {
+    "revision": "b9d9d050401a2fe29f8e2c10b0e9a657",
+    "url": "/static/media/simon.b9d9d050.webp"
+  },
+  {
+    "revision": "0d3569247e8eb3e6eb30985b68204e17",
+    "url": "/static/media/password-generator.0d356924.webp"
+  },
+  {
+    "revision": "3d430ca53b5e777e72f4554f76e8b836",
+    "url": "/static/media/movieapp.3d430ca5.webp"
+  },
+  {
+    "revision": "938c081aed2e73d3159a1b66ed1a709f",
+    "url": "/static/media/photography-website.938c081a.webp"
+  },
+  {
+    "revision": "5da5c6f0d4685b6ae5d84eb08d5179e6",
+    "url": "/static/media/randomstudentpicker.5da5c6f0.webp"
+  },
+  {
+    "revision": "a6a96dd13891194f944a5fc58b8a5b5c",
+    "url": "/static/media/ToDoApp.a6a96dd1.webp"
+  },
+  {
+    "revision": "b67dcd250ab01469c30aa655fc09d300",
+    "url": "/static/media/ThemeClock.b67dcd25.webp"
+  },
+  {
+    "revision": "467e1427cd0a10b0829a09fc1dcd0c44",
+    "url": "/static/media/Notes.467e1427.webp"
+  },
+  {
+    "revision": "bebcfd2348c01a16aa2197f3d4bd8faf",
+    "url": "/index.html"
+  }
+];
